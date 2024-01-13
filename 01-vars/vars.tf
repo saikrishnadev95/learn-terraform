1e3f728840491eaad9dde17f0d8d1308f5c42a8e
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-01-13 13:33:41 +0530

    terraform changes

--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -13,3 +13,38 @@ variable "sample_boolean" {
   default = true
 }
 
+## Booleans are true or false
+
+# Booleans and numbers does not need to be quoted, Only strings need to be quoted that to be with double quotes. Terraform does support for Single quotes.
+
+### Variable Types
+# So far we have seen is default variable type
+
+# We have list variable type , Meaning single variable can hold multiple values
+variable "sample_list" {
+  default = [
+    100,
+    "hello",
+    true,
+    123
+  ]
+}
+
+## Data type inside the list does not matter in terraform
+
+# We have a dictionary (or map), Meaning single variable will  have multiple values, & Each value is going to have a saperate name.
+
+variable "sample_dict" {
+  default = {
+    number1 = 100
+    string1 = "hello"
+    number2 = 123
+    boolean = true
+  }
+}
+
+## This is the most preferred variable over the list when we declare the data.
+
+variable "env" {}
+variable "auto_num1" {}
+variable "sample1" {}
