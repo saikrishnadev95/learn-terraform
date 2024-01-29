19b232de133dbd04ef1e517f71e53bdf0505c97d
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-01-29 14:18:41 +0530

    o

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -1,42 +1,6 @@
-### Convert lower case to upper case
-#variable "sample" {
-#  default = "abcxyz"
-#}
-#
-#output "sample" {
-#  value = upper(var.sample)
-#}
-#
-#variable "sample1" {
-#  default = ["abc", "xyz"]
-#}
-#
-#variable "sample2" {
-#  default = {
-#    xyz = 100
-#    abc = 200
-#  }
-#}
-#
-### Print first value like var.sample1[0] with a function
-#
-#output "sample1" {
-#  value = element(var.sample1, 0)
-#}
-#
-#output "sample2" {
-#  value = lookup(var.sample2, "a1", "dummy")
-#}
-
-
-
-
-
-
-## convert lower case upper case
-
+## Convert lower case to upper case
 variable "sample" {
-  default = "abc"
+  default = "abcxyz"
 }
 
 output "sample" {
@@ -44,17 +8,22 @@ output "sample" {
 }
 
 variable "sample1" {
-  default = ["abc , xyz"]
+  default = ["abc", "xyz"]
 }
 
 variable "sample2" {
   default = {
     xyz = 100
-    abc =200
+    abc = 200
   }
 }
 
+## Print first value like var.sample1[0] with a function
 
+output "sample1" {
+  value = element(var.sample1, 0)
+}
 
-
-
+output "sample2" {
+  value = lookup(var.sample2, "a1", "dummy")
+}
